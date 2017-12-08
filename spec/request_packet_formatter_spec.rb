48ea7f9775e37e2c558487637a120a50ba2f2c73
Michael Sullivan <regularmike@gmail.com>
2017-12-08 09:50:32 -0500

    add response factory spec

--- spec/request_packet_formatter_spec.rb
+++ spec/request_packet_formatter_spec.rb
@@ -22,16 +22,19 @@ module RtsApi
       it "should add a ShowAvalTickets node when first parameter is true" do
         packet = formatter.performance_schedule(true)
         expect(packet.at('ShowAvalTickets')).to_not be_nil
+        expect(packet.at('ShowAvalTickets').text).to eql '1'
       end
       
       it "should add a ShowSales node when second parameter is true" do
         packet = formatter.performance_schedule(false, true)
         expect(packet.at('ShowSales')).to_not be_nil
+        expect(packet.at('ShowSales').text).to eql '1'
       end
 
       it "should add a ShowSaleLinks node when third parameter is true" do
         packet = formatter.performance_schedule(false, false, true)
         expect(packet.at('ShowSaleLinks')).to_not be_nil
+        expect(packet.at('ShowSaleLinks').text).to eql '1'
       end
 
     end
---
+++ spec/response_factory_spec.rb
@@ -0,0 +1,23 @@
+require_relative '../lib/rts_api.rb'
+
+module RtsApi
+
+  describe ResponseFactory do
+
+    before(:each) do  
+      @res = double("res", :code => 200, :body => '<Response></Response>')
+    end
+    
+    it "should create a PerformanceSchedule when command is :performance_schedule" do
+      response = ResponseFactory.create(:performance_schedule, @res)
+      expect(response).to be_an_instance_of PerformanceSchedule
+    end
+
+    it "should create a generic Response when command is unknown" do
+      response = ResponseFactory.create(:some_unknown_command, @res)
+      expect(response).to be_an_instance_of Response
+    end
+  
+  end
+
+end
