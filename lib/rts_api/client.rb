e847b6248c6f89cf89cc3e8d6396e38b6043d1f5
Michael Sullivan <regularmike@gmail.com>
2018-02-06 14:00:27 -0500

    apply RuboCop suggestions to client.rb

--- lib/rts_api/client.rb
+++ lib/rts_api/client.rb
@@ -3,6 +3,8 @@ require 'nokogiri'
 require 'logger'
 
 module RtsApi
+  # This class is used to create a client that can send commands
+  # to an RTS server and parse the responses
   class Client
     # it's unclear what <Version>1</Version> refers to
     # but it's the first child node in every request packet
@@ -13,38 +15,36 @@ module RtsApi
       username: 'test',
       password: 'test',
       formatter: RequestPacketFormatter.new(VERSION),
-      logger: nil)
+      logger: nil
+    )
 
       @url = url
       @username = username
       @password = password
-      @formatter = formatter       
+      @formatter = formatter
       @logger = logger
-      unless @logger
-        @logger = ::Logger.new STDOUT
-        @logger.level = ::Logger::WARN
-      end
+      return if @logger
+      @logger = ::Logger.new STDOUT
+      @logger.level = ::Logger::WARN
     end
 
     # assume missing methods correspond to API "commands",
     # which should have corresponding methods in the packet formatter
     #
-    def method_missing(command, *args, &block)
-      begin
-        request_packet = @formatter.send(command, *args)
-        request = Request.new(request_packet, command)
-      rescue NoMethodError
-        # log an error if the missing method was an attempted API command
-        unless @formatter.respond_to?(command)
-          @logger.error("The '#{command}' API command does not exist or is not supported by this library.") 
-        end
-
-        super
-      end
-      
+    def method_missing(command, *args)
+      return super unless respond_to_missing?(command)
+      request_packet = @formatter.send(command, *args)
+      request = Request.new(request_packet, command)
       res = get_response_with_logging(request)
       yield res if block_given?
       res
+    end
+
+    def respond_to_missing?(method, include_private = false)
+      %w[
+        performance_schedule
+        gift_loyalty_card_information
+      ].include?(method.to_s) || super
     end
 
     private
@@ -55,7 +55,7 @@ module RtsApi
       req.basic_auth @username, @password
       req.content_type = 'text/xml'
       req.body = request.packet.to_s
-      res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => true) do |http|
+      res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
         http.request(req)
       end
 
@@ -66,7 +66,7 @@ module RtsApi
       @logger.info("Sending #{request.command} command.")
       @logger.debug("Request packet:\n #{request.packet}")
       begin
-        response = get_response(request) 
+        response = get_response(request)
         @logger.info("Response HTTP status code: #{response.http_status_code}")
         @logger.debug("Response:\n #{response.packet}")
       rescue StandardError => error
